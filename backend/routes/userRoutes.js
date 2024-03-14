27248bb8dd7602e94421ae9bcd744007f8bd9a6e
amjad78678 <81846358+amjad78678@users.noreply.github.com>
2024-03-14 14:11:20 +0530

    before generate JWT token register successfull

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,17 @@
-import express from 'express';
-const router=express();
-import { authUser } from '../controllers/userController.js'
+import express from "express";
+const router = express();
+import {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+} from "../controllers/userController.js";
+    
+router.post("/",registerUser);
+router.post("/auth", authUser);
+router.post("/logout", logoutUser);
+router.route('/profile').get(getUserProfile).put(updateUserProfile);
 
 
-router.post('/auth',authUser);
-
-export default router;
+export default router
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv'
-dotenv.config();
 import userRoutes from './routes/userRoutes.js'
+import { notFound,errorHandler } from './middleware/errorMiddleware.js';
+import connectDb from './config/db.js';
+dotenv.config();
 
-
-
+connectDb();
 const port=process.env.PORT|| 3000;
 const app = express();
 
-
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 app.use('/api/users/',userRoutes)
-app.get('/',(req,res)=>res.send('server is ready'))
+app.use(notFound);
+app.use(errorHandler)
+
+
 
 app.listen(port,()=>console.log(`listening on port http://localhost:${port}/`));
